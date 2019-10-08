fdd9dee339ba0dc6531ce82b75bfce7f2cec190e
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-10-08 23:19:39 +0630

    Demo7

--- Basic/Demo7.php
+++ Basic/Demo7.php
@@ -15,11 +15,10 @@
             <div class="card bg-light p-5">
 
                 <h3 class="text-info text-center mb-3">Login To System</h3>
-                <!--  !Login form start!-->
-                <form action="" method="post">
+                <form  action="" method="post">
                     <div class="form-group">
                         <label for="exampleInputEmail1" class="english">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="txt_username">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="txt_email">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1" class="english">Password</label>
@@ -28,7 +27,7 @@
 
 
                     <div class="row justify-content-between no-gutters">
-                        <i href="#" class="english"> <?php validate();?></i>
+                        <i href="#" class="english" style="font-style:normal; "> <?php validate();?></i>
                         <div>
 
                             <button type="reset" class="btn btn-outline-secondary english">Cancel</button>
@@ -44,7 +43,6 @@
 
     </div>
 </div>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -56,7 +54,7 @@
 <?php
 function validate(){
     if (isset($_POST["btn_login"])) {
-        $name = $_POST["txt_username"];
+        $name = $_POST["txt_email"];
         $password = $_POST["txt_password"];
 
         $true_name="Admin@gmail.com";
@@ -65,17 +63,18 @@ function validate(){
         if ($true_name==$name ){
             if ($true_password==$password)
             {
-                echo "<font style='color: green'>  User name and Password correct</font>";
+
+                echo "<span  class='badge badge-success'> Email and Password correct</span>";
             }
             else{
-                echo "<font style='color: red'> User name is correct and Password incorrect</font>";
+                echo "<span class='badge badge-danger'> Email is correct and Password incorrect</span>";
             }
         }elseif ($true_name!=$name){
             if ($true_password==$password)
             {
-                echo "<font style='color: red'>User name is incorrect and Password correct</font>";
+                echo "<span class='badge badge-danger'>Email is incorrect and Password correct</span>";
             }else{
-                echo "<font style='color: red'>User name and password is incorrect</font>";
+                echo "<span class='badge badge-danger'>Email and password is incorrect</span>";
             }
         }
     }
