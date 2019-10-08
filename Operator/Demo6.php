5fea2f895f39e01aca9c0e6ba2fb1faf940637b0
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-10-08 14:54:51 +0630

    Demo6

---
+++ Operator/Demo6.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo3</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<h4 style="text-align: center">Create Login Page with PHP Code</h4>
+<div class="container">
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="formGroupExampleInput">Name</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Name" name="txt_username">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput2">Password</label>
+            <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Enter Password" name="txt_password">
+        </div>
+        <button class="btn btn-success" type="submit" name="btn_login">Submit</button>
+        <button class="btn btn-warning text-light" type="reset" >Clear</button>
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+<?php
+if (isset($_POST["btn_login"])) {
+    $name = $_POST["txt_username"];
+    $password = $_POST["txt_password"];
+
+    $true_name="Admin";
+    $true_password="123";
+
+    if ($true_name==$name && $true_password==$password){
+        echo "Login Success";
+    }else{
+        echo "Login Fail";
+    }
+}
+?>
+
+
+
+
