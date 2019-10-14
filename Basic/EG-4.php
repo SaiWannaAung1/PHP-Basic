4e77f372b604cb10e3d3f53de990190d9d3af9d3
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-10-14 23:38:23 +0630

    Commit!

---
+++ Basic/EG-4.php
@@ -0,0 +1,4 @@
+<?php
+$text = "Inle Lake is a freshwater lake located in the Nyaungshwe Township of Taunggyi District of Shan State, part of Shan Hills in Myanmar (Burma).";
+echo "String length is ".strlen($text)."<br>";
+echo "Word is ".str_word_count($text);
