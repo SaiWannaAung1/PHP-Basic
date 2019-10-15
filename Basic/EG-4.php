7eb8ffb40ab5902f42a52e42b7fc9cd4d9162d0a
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-10-15 23:01:47 +0630

    Commit!

--- Basic/EG-4.php
+++ Basic/EG-4.php
@@ -1,6 +1,18 @@
 <?php
-$text = "Inle Lake is a freshwater lake located in the Nyaungshwe Township of Taunggyi District of Shan State, part of Shan Hills in Myanmar (Burma).";
+$text = "Inle Lake is a freshwater lake located in the Nyaungshwe Township of Taunggyi District of Shan State, part of Shan Hills in Myanmar ( Burma).";
 echo "String length is ".strlen($text)."<br>";
 echo "Word is ".str_word_count($text)."<br>";
 echo "First Is Upper :".ucwords(strtolower($text))."<br>";
-echo "fIRST iS lOWER :".(strtoupper($text));
+echo "fIRST iS lOWER :".strtoupper($text);echo "String length is ".strlen($text)."<br>";
+echo "Word is ".str_word_count($text)."<br>";
+echo "First Is Upper :".ucwords(strtolower($text))."<br>";
+echo "fIRST iS lOWER :".strtoupper($text);echo "String length is ".strlen($text)."<br>";
+echo "Word is ".str_word_count($text)."<br>";
+echo "First Is Upper :".ucwords(strtolower($text))."<br>";
+echo "fIRST iS lOWER :".strtoupper($text);echo "String length is ".strlen($text)."<br>";
+echo "Word is ".str_word_count($text)."<br>";
+echo "First Is Upper :".ucwords(strtolower($text))."<br>";
+echo "fIRST iS lOWER :".strtoupper($text);echo "String length is ".strlen($text)."<br>";
+echo "Word is ".str_word_count($text)."<br>";
+echo "First Is Upper :".ucwords(strtolower($text))."<br>";
+echo "fIRST iS lOWER :".strtoupper($text);
