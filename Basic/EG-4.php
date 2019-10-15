67f4728bd13431a3e38cf0c63cbc3b742363f240
SaiWannaAung1 <saiwannaaung095@gmail.com>
2019-10-15 23:23:38 +0630

    Commit!

--- Basic/EG-4.php
+++ Basic/EG-4.php
@@ -1,6 +1,19 @@
 <?php
-$text = "Inle Lake is a freshwater lake located in the Nyaungshwe Township of Taunggyi District of Shan State, part of Shan Hills in Myanmar ( Burma).";
-echo "String length is ".strlen($text)."<br>";
-echo "Word is ".str_word_count($text)."<br>";
-echo "First Is Upper :".ucwords(strtolower($text))."<br>";
-echo "fIRST iS lOWER :".strtoupper($text);
+//$text = "Inle Lake is a freshwater lake located in the Nyaungshwe Township of Taunggyi District of Shan State, part of Shan Hills in Myanmar ( Burma).";
+//echo "String length is ".strlen($text)."<br>";
+//echo "Word is ".str_word_count($text)."<br>";
+//echo "First Is Upper :".ucwords(strtolower($text))."<br>";
+//echo "fIRST iS lOWER :". strtoupper($text);
+class Test{
+
+}
+$a = new Test();
+$b = new Test();
+$a = 128;
+$b = 128;
+if ($a==$b)
+{
+    echo "Null = Null";
+}else{
+    echo "Null != Null";
+}
