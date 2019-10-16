624b9c02a5aa546f1af24e7acbb66f6da15f80ac
SaiWannaAung <saiwannaaung095@gmail.com>
2019-10-16 23:55:57 +0630

    Commit!

--- Basic/Demo1.php
+++ Basic/Demo1.php
@@ -3,5 +3,6 @@ $dob =2001;
 $current = 2019;
 $age = $current-$dob;
 echo "Age is $age!";
+echo "$dob";
 
 ?>
--- Basic/Demo9.php
+++ Basic/Demo9.php
@@ -1,8 +1,60 @@
-<?php 	$foo='hello world';
-echo ucfirst($foo);
-$bar='HELLO WORLD';
-echo "<br>";
-echo ucfirst($bar);
-echo "<br>";
-echo ucfirst(strtolower($bar)); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table width="1000px">
+    <tr>
+        <th>ID</th>
+        <th colspan="1">Product Name</th>
+        <th colspan="1">Price</th>
+        <th colspan="1">Quantity</th>
+        <th colspan="1">Amount</th>
+    </tr>
+    <tr>
+    <td colspan="1" style="text-align: center">1</td>
+    <td colspan="1" style="text-align: center">Water</td>
+    <td colspan="1" style="text-align: center">$1</td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+
+    </tr><tr>
+
+
+    </tr><tr>
+    <td colspan="1" style="text-align: center">2</td>
+    <td colspan="1" style="text-align: center">Milk</td>
+    <td colspan="1" style="text-align: center">$2</td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+
+    </tr><tr>
+    <td colspan="1" style="text-align: center">3</td>
+    <td colspan="1" style="text-align: center">Ma Ma</td>
+    <td colspan="1" style="text-align: center">$1</td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+
+    </tr><tr>
+    <td colspan="1" style="text-align: center">4</td>
+    <td colspan="1" style="text-align: center">Ve Ve</td>
+    <td colspan="1" style="text-align: center">$1</td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+
+    </tr>
+    <td colspan="1" style="text-align: center">5</td>
+    <td colspan="1" style="text-align: center">Pencil</td>
+    <td colspan="1" style="text-align: center">$1</td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+    <td colspan="1" style="text-align: center"> <input type="text"></td>
+
+</table>
+</body>
+</html>
 
